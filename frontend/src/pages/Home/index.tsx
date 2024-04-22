90e489a7c1041b38c1ab36f9ea232b82eae1417c
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-04-22 19:32:47 +0300

    refactor structure & test components

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import styles from './home.module.css';
+
+function Home() {
+  return (
+    <nav>
+      <div className={styles.header}>
+        <h1>Hello From Home</h1>
+      </div>
+      <p>jodsjflks</p>
+    </nav>
+  );
+}
+
+export default Home;
