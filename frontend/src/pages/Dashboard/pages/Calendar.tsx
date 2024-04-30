32ba4eb070c21260cdd592ebb5ff74fa43bb0bfe
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-01 00:46:39 +0300

    Added Calendar

--- frontend/src/pages/Dashboard/pages/Calendar.tsx
+++ frontend/src/pages/Dashboard/pages/Calendar.tsx
@@ -5,7 +5,20 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 
 const Calendar = () => {
-  return <div>Calendar</div>;
+  return (
+    <section className="w-[80%] mx-auto mt-5">
+      <Fullcalendar
+        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+        initialView={'dayGridMonth'}
+        headerToolbar={{
+          start: 'prev,today,next',
+          center: 'title',
+          end: 'dayGridMonth,timeGridWeek,timeGridDay',
+        }}
+        height="85vh"
+      />
+    </section>
+  );
 };
 
 export default Calendar;
