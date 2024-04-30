db0642a33e7bbe042da2c2803abe026e6dec251c
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-04-30 23:58:55 +0300

    Install necessary packages

--- frontend/src/pages/Dashboard/pages/Calendar.tsx
+++ frontend/src/pages/Dashboard/pages/Calendar.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import Fullcalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 const Calendar = () => {
   return <div>Calendar</div>;
