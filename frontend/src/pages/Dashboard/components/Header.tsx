acc73e06d3aa00231153fb5ae843ba0bff471db9
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-01 16:04:36 +0300

    Create and Style Header Component

--- frontend/src/pages/Dashboard/components/Header.tsx
+++ frontend/src/pages/Dashboard/components/Header.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import { Editor } from 'primereact/editor';
 
-const Header = () => {
-  return <div>Header</div>;
+type HeaderProps = {
+  title: string;
+  category: string;
+};
+
+const Header = ({ title, category }: HeaderProps) => {
+  return (
+    <div className="mb-10">
+      <p className="text-gray-400">{category}</p>
+      <p className="font-extrabold text-secondary">{title}</p>
+    </div>
+  );
 };
 
 export default Header;
