846be82ed4fbd56f39e81a8d9f20ddbee2943675
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-02 09:23:39 +0300

    Remove unnecessary classes

--- frontend/src/pages/Home/components/Navbar.tsx
+++ frontend/src/pages/Home/components/Navbar.tsx
@@ -91,9 +91,7 @@ const Navbar = () => {
           transition={{ type: 'spring', stiffness: 260, damping: 20 }}
           className={`w-full p-6 ${shortStyles.flexBetween} bg-gray-800 fixed top-0 left-0 right-0 h-20 border-lightBlue text-white navbar z-40`}
         >
-          <p
-            className={`${styles.navbar} font-bold ${shortStyles.linksFonts} group hover:text-primary text-lg xs:text-xl ss:text-2xl  sm:text-3xl md:text-4xl`}
-          >
+          <p className={`font-bold  group hover:text-primary text-lg xs:text-xl ss:text-2xl  sm:text-3xl md:text-4xl`}>
             Unity<span className={`text-primary group-hover:text-white`}>Pulse</span>
           </p>
 
@@ -112,18 +110,18 @@ const Navbar = () => {
           <div className="sm:hidden flex flex-1 justify-end items-center">
             {toggle ? (
               <button>
-                <Close onClick={handleToggle} className={styles.icons} />
+                <Close onClick={handleToggle} />
               </button>
             ) : (
               <button onClick={handleToggle}>
-                <Menu className={styles.icons} />
+                <Menu />
               </button>
             )}
 
             <ul className={`${toggle ? 'flex' : 'hidden'} absolute inset-0 flex flex-col top-20 z-10`}>
               {navLinks.map((item) => (
                 <Link key={item.id} to={item.section} spy={true} smooth={true} duration={500} offset={-70}>
-                  <li className="bg-secondary bg-opacity-40 text-center border-b-2 border-opacity-60 bg-blue-300 text-secondary font-semibold py-4 border-b-lightBlue hover:bg-opacity-100 cursor-pointer font-semibold text-lg">
+                  <li className="bg-secondary bg-opacity-40 text-center border-b-2 border-opacity-60  text-secondary font-semibold py-4 border-b-lightBlue hover:bg-opacity-100 cursor-pointer text-lg">
                     {item.title}
                   </li>
                 </Link>
