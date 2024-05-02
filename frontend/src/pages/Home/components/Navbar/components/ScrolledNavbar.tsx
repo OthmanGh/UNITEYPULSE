ac3a327e7778d59c2d1adeef1d187c03932f00e3
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-02 13:39:44 +0300

    Fix scrolled navbar styles

--- frontend/src/pages/Home/components/Navbar/components/ScrolledNavbar.tsx
+++ frontend/src/pages/Home/components/Navbar/components/ScrolledNavbar.tsx
@@ -13,7 +13,7 @@ const ScrolledNavbar = ({ handleToggle, toggle }: { toggle: boolean; handleToggl
     initial={{ y: -100, opacity: 0 }}
     animate={{ y: 0, opacity: 1 }}
     transition={{ type: 'spring', stiffness: 260, damping: 20 }}
-    className={`w-full px-6 ${shortStyles.flexBetween} bg-dark fixed top-0 left-0 right-0  text-white z-30 gap-8 md:gap-12 h-[70px]`}
+    className={`w-full px-6 ${shortStyles.flexBetween} bg-dark fixed top-0 left-0 right-0  text-white z-30 gap-8 md:gap-12 h-[70px] border-b-primary border-b-[2px]`}
   >
     <LogoTitle />
 
@@ -25,13 +25,15 @@ const ScrolledNavbar = ({ handleToggle, toggle }: { toggle: boolean; handleToggl
 
     <LoginBtn handleToggle={handleToggle} />
 
-    <div className="sm:hidden flex flex-1 justify-end items-center">
-      <button onClick={handleToggle}>{toggle ? <Close /> : <Menu />}</button>
+    <div className="sm:hidden flex flex-1 justify-end items-center ">
+      <button className="hover:text-primary text-xl transition-all duration-400" onClick={handleToggle}>
+        {toggle ? <Close /> : <Menu />}
+      </button>
 
-      <ul className={`${toggle ? 'flex' : 'hidden'} absolute inset-0 flex flex-col top-20 z-10`}>
+      <ul className={`${toggle ? 'flex' : 'hidden'} absolute inset-0 flex flex-col top-[70px] z-10`}>
         {navLinks.map((item) => (
           <Link key={item.id} to={item.section} spy={true} smooth={true} duration={500} offset={-70}>
-            <li className="bg-secondary bg-opacity-40 text-center border-b-2 border-opacity-60  text-secondary font-semibold py-4 border-b-lightBlue hover:bg-opacity-100 cursor-pointer text-lg">
+            <li className="bg-dark bg-opacity-90 text-center border-b-2 p-4 text-primary border-b-primary hover:scale-110 transition-all duration-400 text-md font-montserrat">
               {item.title}
             </li>
           </Link>
