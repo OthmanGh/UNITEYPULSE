2ee3bdd8433e4174ea6681ff7c115970ea6dfe2c
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-03 08:35:25 +0300

    Fix database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,18 +2,14 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config({ path: "config.env" });
 
-console.log(process.env.MONGODB_URI);
-
 const connect = () => {
-  mongoose.connect(process.env.MONGODB_URI);
+  try {
+    mongoose.connect(process.env.MONGODB_URI);
 
-  mongoose.connection.once("connected", () => {
     console.log("MONGODB connected");
-  });
-
-  mongoose.connection.on("error", error => {
-    console.log(`MONGODB ERROR ${error}`);
-  });
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
 };
 
 export default connect;
