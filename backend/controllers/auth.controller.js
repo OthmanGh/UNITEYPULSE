2802e44824120a2d250715638b339d7794108780
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-03 09:10:02 +0300

    Implement JWT for signing up users

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,22 @@
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   try {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm
+    });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRESIN
+    });
 
     res.status(201).json({
       status: "success",
+      token,
       data: {
         user: newUser
       }
