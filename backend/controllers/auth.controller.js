968438f37b3f08e6d0edcd22e3b4d21b6060ad31
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-03 08:36:10 +0300

    Modify user routes & controllers

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,39 +1,18 @@
 import User from "../models/user.model.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   try {
-    const { name, username, password, confirmPassword, email } = req.body;
+    const newUser = await User.create(req.body);
 
-    if (password !== confirmPassword)
-      return res.status(400).json({
-        error: "Password don't match"
-      });
-
-    const user = await user.findOne({ username });
-    const existingEmail = await user.findOne({ email });
-
-    if (user)
-      return res.status(400).json({
-        error: "username already exist"
-      });
-
-    if (existingEmail)
-      return res.status(400).json({
-        error: "Email already exist"
-      });
-
-    const profilePic =
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTs4XdD00sHtFKBYeyzKvz1CUHr598N0yrUA&s";
-
-    const newUser = new User({
-      name,
-      username,
-      password,
-      email,
-      profilePic
+    res.status(201).json({
+      status: "success",
+      data: {
+        user: newUser
+      }
     });
   } catch (error) {
-    console.log(error);
+    console.error("Error in signup", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,12 +1,10 @@
-import e from "express";
 import { signup, login, logout } from "../controllers/auth.controller.js";
+import e from "express";
 
 const router = e.Router();
 
 router.post("/login", login);
-
 router.post("/signup", signup);
-
 router.post("/logout", logout);
 
 export default router;
