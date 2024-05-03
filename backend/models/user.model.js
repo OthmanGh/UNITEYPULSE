f184979ed1d955833a08e423b7e1ecc390dadff4
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-03 08:34:49 +0300

    Modify user model & hash password

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,33 +1,63 @@
 import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: [true, "Name is required"]
   },
 
-  userName: {
+  username: {
+    // Changed from userName to username
     type: String,
     required: true,
     unique: true
-  },
-
-  password: {
-    type: String,
-    required: true,
-    minlength: 8
   },
 
   email: {
     type: String,
-    required: true,
-    unique: true
+    required: [true, "Email is required"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Provide a valid email"]
+  },
+
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: 8
+  },
+
+  passwordConfirm: {
+    type: String,
+    required: [true, "Confirm password is required"],
+    validate: {
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: "Passwords must match"
+    }
   },
 
   profilePicture: {
     type: String,
     default: ""
+  },
+
+  role: {
+    type: String,
+    enum: ["employee", "business owner", "project manager"]
   }
+});
+
+userSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
