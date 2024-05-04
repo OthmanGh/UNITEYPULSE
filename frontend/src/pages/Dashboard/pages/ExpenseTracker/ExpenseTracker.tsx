884cbef8b033d98033ba4cf7efd656eaa5483fa3
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-05 00:48:49 +0300

    <Frontend> - Create expense page header

--- frontend/src/pages/Dashboard/pages/ExpenseTracker/ExpenseTracker.tsx
+++ frontend/src/pages/Dashboard/pages/ExpenseTracker/ExpenseTracker.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import styles from '../../../../components';
+import Header from '../../components/Header';
+import { AddIcon } from '../../../../utils/icons';
 
 const ExpenseTracker = () => {
-  return <div></div>;
+  return (
+    <section className={styles.dashboardSection}>
+      <div className="flex flex-col  xs:flex-row items-center justify-between">
+        <Header category="app" title="Expense Tracker" />
+        <button className="flex items-center gap-2 bg-secondary text-gray-100 p-3 rounded-md cursor-pointer hover:bg-dark transition-all duration-400">
+          <AddIcon className="text-xl" />
+          Add Expense
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default ExpenseTracker;
