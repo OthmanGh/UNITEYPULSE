c1e535213360fe259f96ca8fa093b46059ed1a4a
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-05 19:06:49 +0300

    <Frontend> - Implement user logouthook

---
+++ frontend/src/hooks/useLogout.ts
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { RequestMethod } from '../services/requestMethods';
+import { useAuthContext } from '../contexts/AuthContext';
+
+const useLogout = () => {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext;
+
+  const logout = async () => {
+    setLoading(true);
+
+    try {
+      const res = await fetch('http://127.0.0.1:8000/api/auth/logout', {
+        method: RequestMethod.post,
+        headers: {
+          'Content-Type:': 'application/json',
+        },
+      });
+
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+
+      localStorage.removeItem('authUser');
+      setAuthUser(null);
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, logout };
+};
+
+export default useLogout;
