63242605bbff709e1f54ab471d61588c9c8a2d5e
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-06 19:47:22 +0300

    <Frontend> - Create custom conversation hook

---
+++ frontend/src/hooks/useGetConversations.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+const useGetConversations = () => {
+  const [loading, setLoading] = useState(false);
+  const [conversations, setConversations] = useState([]);
+
+  useEffect(() => {
+    const getConversations = async () => {
+      setLoading(true);
+
+      try {
+        const res = await fetch('http://127.0.0.1:8000/api/users');
+        const data = await res.json();
+
+        if (data.error) {
+          throw new Error(data.error);
+        }
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getConversations();
+  }, []);
+
+  return { loading, conversations };
+};
+
+export default useGetConversations;
