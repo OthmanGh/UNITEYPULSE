15766378bb39f6f34e364aac42ad6bfc8fd8c1d5
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-07 14:29:56 +0300

    <Frontend> - Implement search conversation

--- frontend/src/pages/Dashboard/pages/Chat/components/SearchInput.tsx
+++ frontend/src/pages/Dashboard/pages/Chat/components/SearchInput.tsx
@@ -1,12 +1,36 @@
+import { useState } from 'react';
 import { IoSearchSharp } from 'react-icons/io5';
+import { useConversation } from '../../../../../contexts/ConversationContext';
+import useGetConversations from '../../../../../hooks/useGetConversations';
 
 const SearchInput = () => {
+  const [search, setSearch] = useState('');
+  const { setSelectedConversation } = useConversation();
+  const { conversations } = useGetConversations();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    const conversation = conversations.find((c) => c.name.toLowerCase().includes(search.toLowerCase()));
+
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch('');
+    } else {
+      console.log('no such user found');
+    }
+  };
+
   return (
-    <form className="flex items-start justify-between gap-2">
+    <form className="flex items-start justify-between gap-2" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Search"
         className="input input-bordered rounded-ml bg-secondary bg-opacity-50 placeholder:text-gray-300 text-white w-[100%] focus:bg-secondary border-none outline-none"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
 
       <button type="submit" className="btn btn-rectangle bg-secondary bg-opacity-55 border-none text-gray-300 text-lg hover:bg-secondary h-[40px]">
