fece7716f8232df34599224b7b6604ee485212bd
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-07 16:39:15 +0300

    <Frontend> - Implement socket context

---
+++ frontend/src/contexts/SocketContext.tsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { useAuthContext } from './AuthContext';
+import io from 'socket.io-client';
+
+const SocketContext = createContext<any>(null);
+
+export const useSocketContext = () => {
+  return useContext(SocketContext);
+};
+
+export const SocketContextProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [socket, setSocket] = useState<any>(null);
+  const [onlineUsers, setOnlineUsers] = useState<any[]>([]);
+  const { authUser } = useAuthContext();
+
+  useEffect(() => {
+    if (authUser) {
+      const socket = io('http://127.0.0.1:5000', {
+        query: {
+          userId: authUser._id,
+        },
+      });
+      setSocket(socket);
+
+      socket.on('getOnlineUsers', (users) => {
+        setOnlineUsers(users);
+      });
+
+      return () => socket.close();
+    } else {
+      if (socket) {
+        socket.close();
+        setSocket(null);
+      }
+    }
+  }, [authUser]);
+
+  return <SocketContext.Provider value={{ socket, onlineUsers }}>{children}</SocketContext.Provider>;
+};
