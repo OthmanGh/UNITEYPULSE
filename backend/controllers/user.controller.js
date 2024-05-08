0b7499ded9fa39bf90cdb2e88396453eb69ba401
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-08 16:19:51 +0300

    <Backend> - fix getting all users for sidebar controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -4,17 +4,13 @@ export const getUsersForSidebar = async (req, res) => {
   try {
     const currentUserId = req.user._id;
 
-    const filteredUsers = await User({
-      _id: {
-        $ne: {
-          currentUserId
-        }
-      }
+    const filteredUsers = await User.find({
+      _id: { $ne: currentUserId }
     });
 
     res.status(200).json(filteredUsers);
   } catch (error) {
-    console.error("Error in getUserForSidebar ", error.message);
+    console.error("Error in getUsersForSidebar ", error.message);
 
     res.status(500).json({ error: "Internal server error" });
   }
