9e9dff0ba1e367509907a63aeba7411fef7d89ec
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-08 19:15:54 +0300

    <Frontend> - Fix conversations container component

---
+++ frontend/src/pages/Dashboard/pages/Chat/components/Conversations.tsx
@@ -0,0 +1,20 @@
+import useGetConversations from '../../../../../hooks/useGetConversations';
+import Conversation from './Conversation';
+
+const Conversations = () => {
+  const { loading, conversations } = useGetConversations();
+
+  return (
+    <div className="py-2 flex flex-col gap-2 bg-dark overflow-auto relative">
+      {loading ? (
+        <span className="loading loading-spinner mx-auto"></span>
+      ) : conversations && conversations.length > 0 ? (
+        conversations.map((conversation, idx) => <Conversation key={conversation._id} conversation={conversation} lastIdx={idx} />)
+      ) : (
+        <p className="absolute z-20 inset-0 text-black text-xl">No Current Users</p>
+      )}
+    </div>
+  );
+};
+
+export default Conversations;
