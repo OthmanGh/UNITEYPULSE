e776d01456dc2fb9c5022a88aadfa4baed82a572
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-08 08:39:25 +0300

    <Frontend> - handle input change

--- frontend/src/pages/Infos/components/Input.tsx
+++ frontend/src/pages/Infos/components/Input.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 type InputProps = {
   name: string;
   placeholder: string;
@@ -7,18 +9,30 @@ type InputProps = {
 };
 
 const Input = ({ placeholder, name, type, register, error }: InputProps) => {
+  const [value, setValue] = useState('');
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
   return (
     <div className="relative w-[80%]">
+      <label htmlFor={name} className="sr-only">
+        {placeholder}
+      </label>
       <span className="absolute right-0 text-red-500">*</span>
 
       <input
+        id={name}
         className="bg-transparent border-b-[1px] px-1 py-3 border-b-secondary text-center outline-none text-secondary placeholder:text-gray-400 h-[50px]  w-[100%]"
         placeholder={placeholder}
         name={name}
         type={type}
+        value={value}
         {...register(name, {
-          required: `${placeholder} is requried`,
+          required: `${placeholder} is required`,
         })}
+        onChange={handleChange}
       />
 
       {error && <span className="text-red-500 text-sm w-full">{error?.message}</span>}
