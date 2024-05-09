e7d05fc217a3dcdfa11f128e15834542841898c8
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-09 08:03:46 +0300

    <Backend> - Create customer model

---
+++ backend/models/customer.model.js
@@ -0,0 +1,53 @@
+// customer.model.js
+import mongoose from "mongoose";
+
+const customerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"]
+    },
+
+    projectName: {
+      type: String,
+      required: [true, "Project name is required"]
+    },
+
+    status: {
+      type: String,
+      enum: ["Pending", "In Progress", "Completed"],
+      default: "Pending"
+    },
+
+    weeks: {
+      type: Number,
+      default: 0
+    },
+
+    budget: {
+      type: Number,
+      default: 0
+    },
+
+    location: {
+      type: String
+    },
+
+    customerId: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+export default Customer;
