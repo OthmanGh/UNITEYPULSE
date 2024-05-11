a7b135995ef29bf38ab1b03e7c9af2426b2bf1d4
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-11 21:02:15 +0300

    <Mobile> - Create onboarding screen & custom button

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,15 +1,33 @@
-import { StatusBar } from 'react-native';
-import { Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import React from 'react';
+import { ScrollView, View, Image, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
+
+import { logo } from '../constants/images';
+import CustomButton from '../components/CustomButton';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Unity Paulse</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: 'blue' }}>
-        Go to Home
-      </Link>
-    </View>
+    <SafeAreaView className="bg-extraDark h-full">
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image source={logo} className="w-[130px] h-[100px]" resizeMode="contain" />
+
+          <View className="relative mt-5">
+            <Text className="text-2xl text-white p-2 font-bold text-center inline-block">
+              Discover Endless Possibilities with <Text className=" text-primary mx-auto inline-block">Unity Paulse</Text>
+            </Text>
+          </View>
+
+          <Text className="text-[12px]  font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovations: embark on a journey of limitless exploration with UnityPaulse
+          </Text>
+
+          <CustomButton title="Continue with Email" handlePress={() => {}} containerStyles="w-full mt-7" />
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor="#042d3e" style="light" />
+    </SafeAreaView>
   );
 }
---
+++ mobile/constants/images.js
@@ -0,0 +1,3 @@
+import logo from '../assets/imgaes/logo_1.png';
+
+export { logo };
