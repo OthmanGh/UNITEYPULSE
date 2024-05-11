d96aaa5a1031627659a08904cc6f2625ac57ef98
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-11 16:45:48 +0300

    <Mobile> - Add assets folder

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,9 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
-
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#64CCC5',
+        'primary-light': '#A6F6F1',
+        secondary: '#176B87',
+        dark: '#042d3e',
+
+        black: {
+          DEFAULT: '#000',
+          100: '#1E1E2D',
+          200: '#232533',
+        },
+        gray: {
+          100: '#CDCDE0',
+        },
+      },
+      fontFamily: {
+        pthin: ['Poppins-Thin', 'sans-serif'],
+        pextralight: ['Poppins-ExtraLight', 'sans-serif'],
+        plight: ['Poppins-Light', 'sans-serif'],
+        pregular: ['Poppins-Regular', 'sans-serif'],
+        pmedium: ['Poppins-Medium', 'sans-serif'],
+        psemibold: ['Poppins-SemiBold', 'sans-serif'],
+        pbold: ['Poppins-Bold', 'sans-serif'],
+        pextrabold: ['Poppins-ExtraBold', 'sans-serif'],
+        pblack: ['Poppins-Black', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
