33682bd3d7367f72a34eb87e2b5ac55e2754b082
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-11 21:53:13 +0300

    <Mobile> - Creating basic strucure for login page

--- mobile/app/(auth)/login.jsx
+++ mobile/app/(auth)/login.jsx
@@ -1,11 +1,18 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Image } from 'react-native';
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { logo } from '../../constants/images';
 
 const Login = () => {
   return (
-    <View>
-      <Text>login</Text>
-    </View>
+    <SafeAreaView className="bg-extraDark h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full my-6 px-4">
+          <Image source={logo} className="w-full h-[150px]" resizeMode="contain" />
+          <Text className="text-2xl text-gray-50 text-semibold mt-0 font-psemibold text-center">login in to unity paulse</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
