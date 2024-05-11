a1945c0fcf9a8c4467ba60069d4d1b2ecdfde7e5
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-11 21:09:46 +0300

    <Mobile> - Implement routing to auth page

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ScrollView, View, Image, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
-
+import { Redirect, router } from 'expo-router';
 import { logo } from '../constants/images';
 import CustomButton from '../components/CustomButton';
 
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <SafeAreaView className="bg-extraDark h-full">
       <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className="w-full justify-center items-center h-full px-4">
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Image source={logo} className="w-[130px] h-[100px]" resizeMode="contain" />
 
           <View className="relative mt-5">
@@ -23,7 +23,7 @@ export default function App() {
             Where creativity meets innovations: embark on a journey of limitless exploration with UnityPaulse
           </Text>
 
-          <CustomButton title="Continue with Email" handlePress={() => {}} containerStyles="w-full mt-7" />
+          <CustomButton title="Continue with Email" handlePress={() => router.push('/login')} containerStyles="w-full mt-7" />
         </View>
       </ScrollView>
 
