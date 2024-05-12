3a39ad96a342b7ce4ca1282e16f8e53c231bd4dd
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-12 16:14:47 +0300

    <Mobile> - Implement login functionality

--- mobile/app/(auth)/login.jsx
+++ mobile/app/(auth)/login.jsx
@@ -1,11 +1,15 @@
-import { View, Text, ScrollView, Image } from 'react-native';
 import React, { useState } from 'react';
+import { View, Text, ScrollView, Image, Alert } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { logo } from '../../constants/images';
 import FormField from '../../components/FormField';
 import CustomButton from '../../components/CustomButton';
+import { API_BASE_URI } from '../../constants/data';
+import { useNavigation } from '@react-navigation/native';
 
 const Login = () => {
+  const navigation = useNavigation();
+
   const [form, setForm] = useState({
     email: '',
     password: '',
@@ -13,7 +17,38 @@ const Login = () => {
 
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const submit = () => {};
+  const submit = async () => {
+    setIsSubmitting(true);
+
+    console.log(form);
+
+    try {
+      const url = `${API_BASE_URI}/auth/login`;
+
+      const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(form),
+      });
+
+      const data = await res.json();
+
+      console.log(data);
+
+      if (data.status === 'success') {
+        navigation.navigate('(tabs)', { screen: 'home' });
+      } else {
+        Alert.alert('Login Failed', data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Error', 'An error occurred. Please try again later.');
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
     <SafeAreaView className="bg-extraDark h-full">
@@ -21,6 +56,7 @@ const Login = () => {
         <View className="w-full justify-center h-[85vh] my-6 px-4">
           <Image source={logo} className="w-full h-[150px]" resizeMode="contain" />
           <Text className="text-2xl text-gray-50 text-semibold mt-0 font-psemibold text-center">Login in to unity paulse</Text>
+
           <FormField
             title="Email"
             value={form.email}
