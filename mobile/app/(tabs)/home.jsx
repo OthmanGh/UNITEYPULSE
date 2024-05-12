248767a57ac76b717ae17ebfe03dd6a5601cebc0
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-12 16:43:16 +0300

    <Mobile> - implement auth context

--- mobile/app/(tabs)/home.jsx
+++ mobile/app/(tabs)/home.jsx
@@ -1,11 +1,17 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import React from 'react';
+
+import { logo } from '../../constants/images';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+      <View className="h-full bg-extraDark relative">
+        <Image source={logo} resizeMode="contain" className="h-[60px] absolute right-[120px] top-6" />
+        <Text></Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
