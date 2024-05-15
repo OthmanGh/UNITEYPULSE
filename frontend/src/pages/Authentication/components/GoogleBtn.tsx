4457ba79cd951a74f02e1306caed400d4cd1d9d0
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-15 14:28:51 +0300

    <Frontend> - Modify Google button type

--- frontend/src/pages/Authentication/components/GoogleBtn.tsx
+++ frontend/src/pages/Authentication/components/GoogleBtn.tsx
@@ -8,6 +8,7 @@ type GoogleBtnProps = {
 const GoogleBtn = ({ onSubmit, isLogin }: GoogleBtnProps) => {
   return (
     <button
+      type="button"
       className="flex gap-4 justify-center items-center text-center mt-[-15px] bg-gray-800 hover:bg-half-transparent bg-opacity-70 text-gray-300 font-semibold w-full mx-auto h-[50px] rounded-md px-8 transition-all duration-300 hover:text-primary hover:text-opacity-90"
       onClick={onSubmit}
     >
