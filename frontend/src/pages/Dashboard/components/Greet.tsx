21e9a6f0a749a4784c3cc554c839d28ad3203de9
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-16 11:00:50 +0300

    <Frontend> - Modify greeting page text dashboard

--- frontend/src/pages/Dashboard/components/Greet.tsx
+++ frontend/src/pages/Dashboard/components/Greet.tsx
@@ -7,12 +7,10 @@ const Greet = () => {
   return (
     <div className="flex items-center justify-center mt-20 w-[60%] mx-auto text-center">
       <h2 className="text-3xl text-gray-400">
-        Welcome <span className="text-secondary font-bold ">Back</span>, To Your Essential Business Toolkit for Growth and Success
+        <span className="text-secondary font-bold">{authUser?.name}</span>, Welcome to Your Essential Business Toolkit for Growth and Success
       </h2>
     </div>
   );
 };
 
 export default Greet;
-
-// {authUser?.name[0].toUpperCase() + authUser?.name.slice(1).toLowerCase()}
