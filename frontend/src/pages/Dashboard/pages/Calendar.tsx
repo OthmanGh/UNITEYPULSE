9fc115e14a447057b686079b67345fe801f0c30f
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-17 18:59:48 +0300

    <Frontend> - Link events

--- frontend/src/pages/Dashboard/pages/Calendar.tsx
+++ frontend/src/pages/Dashboard/pages/Calendar.tsx
@@ -1,14 +1,45 @@
-import React from 'react';
-import Fullcalendar from '@fullcalendar/react';
+import React, { useState, useEffect } from 'react';
+import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import styles from '../../../components';
+import axios from 'axios';
 
 const Calendar = () => {
+  const [events, setEvents] = useState([]);
+
+  const fetchEvents = async () => {
+    try {
+      const token = JSON.parse(localStorage.getItem('authUser')).token;
+
+      const response = await axios.get('http://127.0.0.1:8000/api/events', {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const formattedEvents = response.data.data.map((event) => ({
+        id: event._id,
+        title: event.title,
+        start: new Date(event.date),
+        end: new Date(event.date),
+      }));
+
+      setEvents(formattedEvents);
+    } catch (error) {
+      console.error('Error fetching events:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
   return (
     <section className={styles.dashboardSection}>
-      <Fullcalendar
+      <FullCalendar
         plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
         initialView={'dayGridMonth'}
         headerToolbar={{
@@ -17,6 +48,21 @@ const Calendar = () => {
           end: 'dayGridMonth,timeGridWeek,timeGridDay',
         }}
         height="85vh"
+        events={events}
+        editable={true}
+        selectable={true}
+        selectMirror={true}
+        dayMaxEvents={true}
+        eventClick={(eventInfo) => {
+          if (window.confirm(`Are you sure you want to remove the event '${eventInfo.event.title}'?`)) {
+          }
+        }}
+        select={(selectionInfo) => {
+          const title = prompt('Please enter a title for your event:');
+          if (title) {
+          }
+        }}
+        eventChange={(eventInfo) => {}}
       />
     </section>
   );
