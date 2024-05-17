3997080475e644c786c861e22067299b17758f72
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-17 08:13:01 +0300

    <Frontend> - Implement create customer hook

---
+++ frontend/src/hooks/useCreateCustomer.ts
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { API_BASE_URI } from '../utils';
+
+const useCreateCustomer = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const createCustomer = async (customerData) => {
+    setLoading(true);
+    try {
+      const token = JSON.parse(localStorage.getItem('authUser')).token;
+      const url = `http://127.0.0.1:8000/api/customers`;
+
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(customerData),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        return data.data.customer;
+      } else {
+        throw new Error(data.message);
+      }
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { createCustomer, loading, error };
+};
+
+export default useCreateCustomer;
