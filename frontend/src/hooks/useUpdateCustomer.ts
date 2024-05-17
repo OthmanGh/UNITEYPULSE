c1c9efaa337b6f71cb90b26f1397179ae9e2f505
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-17 12:14:04 +0300

    <Frontend> - Create update customer hook

---
+++ frontend/src/hooks/useUpdateCustomer.ts
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { API_BASE_URI } from '../utils';
+
+const useUpdateCustomer = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const updateCustomer = async (customerId, customerData) => {
+    setLoading(true);
+    try {
+      const token = JSON.parse(localStorage.getItem('authUser')).token;
+      const url = `http://127.0.0.1:8000/api/customers/${customerId}`;
+
+      const response = await fetch(url, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(customerData),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        return data.data.customer;
+      } else {
+        throw new Error(data.message);
+      }
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { updateCustomer, loading, error };
+};
+
+export default useUpdateCustomer;
