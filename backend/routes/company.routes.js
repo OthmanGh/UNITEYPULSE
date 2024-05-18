690dc77d1f63f878bb1f570aead92545249bde19
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-18 15:53:32 +0300

    <Backend> - Create company routes

---
+++ backend/routes/company.routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  createCompany,
+  getAllCompanies,
+  getCompanyById,
+  updateCompany,
+  deleteCompany
+} from "../controllers/companyController.js";
+
+const router = express.Router();
+
+router.post("/", createCompany);
+router.get("/:id", getCompanyById);
+router.put("/:id", updateCompany);
+router.delete("/:id", deleteCompany);
+
+export default router;
