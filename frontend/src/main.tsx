6906137186bf4b56422bfb1f2b88a5f80b94f44a
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-18 16:22:49 +0300

    <Frontend> - Wrap app with company provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
@@ -8,15 +7,18 @@ import { ContextProvider } from './contexts/ContextProvider.tsx';
 import { AuthContextProvider } from './contexts/AuthContext.tsx';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { SocketContextProvider } from './contexts/SocketContext.tsx';
+import { CompanyProvider } from './contexts/CompanyContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <GoogleOAuthProvider clientId="1089419800008-tonr18jq1vahjf74ikge8ub7imen0c1i.apps.googleusercontent.com">
     <AuthContextProvider>
-      <ContextProvider>
-        <SocketContextProvider>
-          <App />
-        </SocketContextProvider>
-      </ContextProvider>
+      <CompanyProvider>
+        <ContextProvider>
+          <SocketContextProvider>
+            <App />
+          </SocketContextProvider>
+        </ContextProvider>
+      </CompanyProvider>
     </AuthContextProvider>
   </GoogleOAuthProvider>
 );
