4b74b780e70991db96a5485cf09248db8bb57938
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-18 22:27:16 +0300

    <Backend> - Modify transactions model

--- backend/models/transaction.model.js
+++ backend/models/transaction.model.js
@@ -2,11 +2,6 @@ import mongoose from "mongoose";
 
 const transactionSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true
-    },
-
     description: {
       type: String,
       required: true
