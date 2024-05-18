2cc79d17c8883b2ff2f39569df6441e811b43df9
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-18 15:21:53 +0300

    <Backend> - Modify editor controller

--- backend/controllers/editor.controller.js
+++ backend/controllers/editor.controller.js
@@ -14,22 +14,11 @@ export const getTextContent = async (req, res) => {
 
 export const createTextContent = async (req, res) => {
   try {
-    const { content, images } = req.body;
+    const { content } = req.body;
     const userId = req.user._id;
-
-    const imageUrls = [];
-    if (images && images.length > 0) {
-      for (const image of images) {
-        const imageName = `${Date.now()}-${image.originalname}`;
-        const imagePath = path.join("public", "images", imageName);
-        await fs.promises.writeFile(imagePath, image.buffer);
-        imageUrls.push(`/images/${imageName}`);
-      }
-    }
 
     const newContent = await TextContent.create({
       content,
-      images: imageUrls,
       user: userId
     });
 
