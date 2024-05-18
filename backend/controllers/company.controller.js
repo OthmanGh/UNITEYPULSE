fc020654c7bad4b12018f9fe1531ec1a631acb90
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-18 15:48:37 +0300

    <Backend> - Implement update company api

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -30,3 +30,53 @@ export const getCompanyById = async (req, res) => {
     handleError(err, "getCompanyById", res);
   }
 };
+
+export const updateCompany = async (req, res) => {
+  const { id } = req.params;
+  const userId = req.user._id;
+
+  try {
+    const company = await Company.findOneAndUpdate(
+      { _id: id, user: userId },
+      req.body,
+      { new: true }
+    );
+
+    if (!company) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Company not found"
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      data: {
+        company
+      }
+    });
+  } catch (err) {
+    handleError(err, "updateCompany", res);
+  }
+};
+
+export const deleteCompany = async (req, res) => {
+  const { id } = req.params;
+  const userId = req.user._id;
+
+  try {
+    const company = await Company.findOneAndDelete({ _id: id, user: userId });
+
+    if (!company) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Company not found"
+      });
+    }
+    res.status(204).json({
+      status: "success",
+      data: null
+    });
+  } catch (err) {
+    handleError(err, "deleteCompany", res);
+  }
+};
