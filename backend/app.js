c1eeb65aa381358987c7ee715e5af655557d6c56
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-18 15:54:30 +0300

    <Backend> - Add company route to main app

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ import chatbotRoutes from "./routes/chatbot.routes.js";
 import eventRoutes from "./routes/events.routes.js";
 import expenseRoutes from "./routes/expense.routes.js";
 import editorRoutes from "./routes/editor.routes.js";
+import companyRoutes from "./routes/companyRoutes.js";
 
 app.use(
   express.json({
@@ -30,6 +31,7 @@ app.use("/api/chatbot", chatbotRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api/expenses", expenseRoutes);
 app.use("/api/editor", editorRoutes);
+app.use("/api/company", companyRoutes);
 
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, err => {
