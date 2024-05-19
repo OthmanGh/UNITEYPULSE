385a40c378f0b37ef9c8a1721527ac7faf3f4459
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-19 08:45:34 +0300

    <Mobile> - Add constant data

--- mobile/constants/data.js
+++ mobile/constants/data.js
@@ -1,3 +1,47 @@
 const API_BASE_URI = 'http://192.168.31.132:8081/api';
 
 export { API_BASE_URI };
+
+export const events = [
+  {
+    name: 'Event 1',
+    id: 1,
+    image:
+      'https://images.unsplash.com/photo-1531973576160-7125cd663d86?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29tcGFueXxlbnwwfHwwfHx8MA%3D%3D',
+  },
+
+  {
+    name: 'Event 2',
+    id: 2,
+    image:
+      'https://images.unsplash.com/photo-1531973576160-7125cd663d86?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29tcGFueXxlbnwwfHwwfHx8MA%3D%3D',
+  },
+
+  {
+    id: 3,
+    name: 'Event 3',
+    image:
+      'https://images.unsplash.com/photo-1560179707-f14e90ef3623?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y29tcGFueXxlbnwwfHwwfHx8MA%3D%3D',
+  },
+
+  {
+    name: 'Event 1',
+    id: 1,
+    image:
+      'https://images.unsplash.com/photo-1531973576160-7125cd663d86?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29tcGFueXxlbnwwfHwwfHx8MA%3D%3D',
+  },
+
+  {
+    name: 'Event 2',
+    id: 2,
+    image:
+      'https://images.unsplash.com/photo-1531973576160-7125cd663d86?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29tcGFueXxlbnwwfHwwfHx8MA%3D%3D',
+  },
+
+  {
+    id: 3,
+    name: 'Event 3',
+    image:
+      'https://images.unsplash.com/photo-1560179707-f14e90ef3623?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y29tcGFueXxlbnwwfHwwfHx8MA%3D%3D',
+  },
+];
