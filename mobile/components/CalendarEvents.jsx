c20f1ca03b46f98429b482b9b963534c090042fc
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-20 22:15:11 +0300

    <Mobile> - Create calendar events component

---
+++ mobile/components/CalendarEvents.jsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { convertToReadableDate } from '../constants/data';
+
+const CalendarEvents = ({ events }) =>
+  events.map((event, index) => (
+    <View key={index} className="bg-dark p-4 mb-2 border-l-2 border-l-primary flex flex-col rounded">
+      <View className="flex flex-row items-center justify-between mb-2">
+        <Text className="text-primary text-[16px]">{event.title}</Text>
+        <Text className="text-primary-light text-[12px]"> {convertToReadableDate(event.date)}</Text>
+      </View>
+
+      <Text className="text-slate-300 mb-2 text-[12px]">{event.description}</Text>
+      <Text className="text-primary-light text-[13px]">{event.type}</Text>
+    </View>
+  ));
+
+export default CalendarEvents;
