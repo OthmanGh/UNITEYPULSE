104acfd202f281d8d82e9e5d59314693cc2d87c0
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-05-21 11:03:54 +0300

    <Mobile> - Remove unnecssary invokation

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -5,29 +5,32 @@ import { StatusBar } from 'expo-status-bar';
 import { logo, onBoardingImage } from '../constants/images';
 import CustomButton from '../components/CustomButton';
 import { router } from 'expo-router';
+import { SocketProvider } from '../Context/SocketContext';
 
 export default function App() {
   return (
     <SafeAreaView className="bg-extraDark h-full">
-      <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className="w-full justify-center items-center h-[100vh] px-4">
-          <Image source={logo} className="w-full h-[150px]" resizeMode="contain" />
+      <SocketProvider>
+        <ScrollView contentContainerStyle={{ height: '100%' }}>
+          <View className="w-full justify-center items-center h-[100vh] px-4">
+            <Image source={logo} className="w-full h-[150px]" resizeMode="contain" />
 
-          <View className="relative">
-            <Text className="text-2xl text-white p-2 font-bold text-center inline-block">
-              Discover Endless Possibilities with <Text className=" text-primary mx-auto inline-block">Unity Pulse</Text>
+            <View className="relative">
+              <Text className="text-2xl text-white p-2 font-bold text-center inline-block">
+                Discover Endless Possibilities with <Text className=" text-primary mx-auto inline-block">Unity Pulse</Text>
+              </Text>
+            </View>
+
+            <Text className="text-[12px]  font-pregular text-gray-100 mt-7 text-center">
+              Where creativity meets innovations: embark on a journey of limitless exploration with UnityPulse
             </Text>
+
+            <CustomButton title="Continue with Email" handlePress={() => router.push('/login')} containerStyles="w-full mt-7" />
           </View>
+        </ScrollView>
 
-          <Text className="text-[12px]  font-pregular text-gray-100 mt-7 text-center">
-            Where creativity meets innovations: embark on a journey of limitless exploration with UnityPulse
-          </Text>
-
-          <CustomButton title="Continue with Email" handlePress={() => router.push('/login')} containerStyles="w-full mt-7" />
-        </View>
-      </ScrollView>
-
-      <StatusBar backgroundColor="#042d3e" style="light" />
+        <StatusBar backgroundColor="#042d3e" style="light" />
+      </SocketProvider>
     </SafeAreaView>
   );
 }
